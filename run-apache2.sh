37f2081c3e2da6e56428cdbc126700e893423363
Gianpaolo Macario <gmacario@gmail.com>
2014-11-16 13:52:23 +0100

    Configure site https://maxlab.polito.it
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- run-apache2.sh
+++ run-apache2.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 # See https://github.com/phusion/baseimage-docker#adding_additional_daemons
+# https://www.digitalocean.com/community/tutorials/how-to-create-a-ssl-certificate-on-apache-for-ubuntu-14-04
 
 set -e
 
-# TODO: Should move the preparation to Dockerfile
+# TODO: Add to Dockerfile
+# ADD xxx /etc/apache2/ssl/apache.pem
+
+# TODO: Should move the preparation to Dockerfile (ADD webproxy.conf)
+# Leave only a webproxy.conf.sample on git repo
+
+# TODO: Parse config file "webproxy.conf" in the following format:
+#
+# KEYWORD    URL
+# -------------------------------------
+# site       https://github.com/gmacario/maxlab.polito.it.git
+# conf       https://raw.githubusercontent.com/gmacario/gm-hostconfig/master/maxlab-webproxy/etc/apache2/sites-available/maxlab
+# conf       https://raw.githubusercontent.com/gmacario/gm-hostconfig/master/maxlab-webproxy/etc/apache2/sites-available/maxlab-ssl
+# site       https://github.com/SOLARMA/solarma.github.com.git
+# conf       https://raw.githubusercontent.com/gmacario/gm-hostconfig/master/maxlab-webproxy/etc/apache2/sites-available/solarma
+# -------------------------------------
+# KEYWORD: {site | conf}
+# URL:     URL where the repo/file can be fetched
 
 a2enmod proxy proxy_http
+a2enmod ssl
 
 # Populate/update contents for site "http://maxlab.polito.it/"
 cd /var/www && if [ ! -e /var/www/maxlab.polito.it ]; then
@@ -26,6 +45,19 @@ if [ ! -e /etc/apache2/sites-available/maxlab.conf ]; then
 // {print $0}
 ' maxlab.conf.ORIG >maxlab.conf
     cd /etc/apache2/sites-enabled && ln -sf ../sites-available/maxlab.conf
+fi
+# Configure site "https://maxlab.polito.it/"
+if [ ! -e /etc/apache2/sites-available/maxlab-ssl.conf ]; then
+    cd /etc/apache2/sites-available
+    curl -o maxlab-ssl.conf.ORIG \
+        https://raw.githubusercontent.com/gmacario/gm-hostconfig/master/maxlab-webproxy/etc/apache2/sites-available/maxlab-ssl
+    awk '
+/DocumentRoot/ {print "\tDocumentRoot /var/www/maxlab.polito.it"; next}
+/SSLCertificateFile/ {print "\tSSLCertificateFile /etc/apache2/ssl/apache.crt"
+        print "\tSSLCertificateKeyFile /etc/apache2/ssl/apache.key"; next}
+// {print $0}
+' maxlab-ssl.conf.ORIG >maxlab-ssl.conf
+    cd /etc/apache2/sites-enabled && ln -sf ../sites-available/maxlab-ssl.conf
 fi
 
 # Unconfigure default site
